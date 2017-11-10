94c20395e012589a9ecab0d03d050e925b127bfc
Marie Houser <joe.and.marie.houser@gmail.com>
2017-11-10 11:40:34 -0600

    Finished project.

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -61,7 +61,7 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -110,7 +110,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.size()>0) {
 
-        System.out.println("printJobs is not implemented yet");
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("*****");
+                for (String jobKey : job.keySet()) {
+                    System.out.println(jobKey + ": " + job.get(jobKey));
+                }
+                System.out.println("*****");
+            }
+        }else{
+            System.out.println("We're sorry; no jobs seem to have matched your search terms.  Maybe you should be less picky.");
+        }
     }
 }
